6f54476fcbebf2c860e30ac547fe362c7173dd40
Kim Lim <kim.lim@Kims-MacBook-Pro.local>
2018-02-01 15:18:07 -0500

    adjusted some spacing for each entry, adjusted responsiveness of entrys

--- client/src/Components/ProductEntry/ProductEntry.js
+++ client/src/Components/ProductEntry/ProductEntry.js
@@ -5,16 +5,14 @@ import thumbnail from '../Profiles/Profile/ProfileIcon/thumbnail.png';
 const ProductEntry = () => {
     return (
         <Aux>
-            <div className="Entry">
-                <div className="row">
-                    <div className="col-xs-3" id="ProductImg">
-                        <img src={thumbnail} alt="..." width="170px" class="rounded-circle" />
-                    </div>
-                    <div className="col-xs-9" id="ProductContent">
-                        <h4 className="ProductName">Product Name</h4>
-                        <p className="ProductSubtitle">Price // Purchase // Promocode</p>
-                        <p className="ProductDescription">Product Description</p>
-                    </div>
+            <div className="row">
+                <div className="col-sm-3" id="ProductImg">
+                    <img src={thumbnail} alt="..." width="170px" class="rounded-circle" />
+                </div>
+                <div className="col-sm-9" id="ProductContent">
+                    <h4 className="ProductName">Product Name</h4>
+                    <p className="ProductSubtitle">Price // Purchase // Promocode</p>
+                    <p className="ProductDescription">Product Description</p>
                 </div>
             </div>
         </Aux>
