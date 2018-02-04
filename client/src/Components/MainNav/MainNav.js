fb6854131e6be6a43210ab2aa314f8db3241edfd
Kim Lim <kim.lim@Kims-MacBook-Pro.local>
2018-02-03 22:36:52 -0500

    added scrollchor for homepage navigation, formatted main nav bar.

--- client/src/Components/MainNav/MainNav.js
+++ client/src/Components/MainNav/MainNav.js
@@ -1,56 +1,45 @@
-import React,{Component} from 'react';
-import { NavLink } from 'react-router-dom';
+import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom';
+import Scrollchor from 'react-scrollchor';
 import './MainNav.css';
-import BurgerMenu from 'react-burger-menu';
-//import MenuWrap from './../MenuWrap/MenuWrap'
-//import BurgerIcon from 'react-burger-menu';
 
 class MainNav extends Component {
-    state = {
-        currentMenu: 'slide',
-        side: 'left',
-
-      };
-    getMenu = () => {
-        const Menu = BurgerMenu[this.state.currentMenu];
-        const items = [
-            <a key="0" href=""><i className="fa fa-fw fa-star-o" /><span>Favorites</span></a>,
-            <a key="1" href=""><i className="fa fa-fw fa-bell-o" /><span>Alerts</span></a>,
-            <a key="2" href=""><i className="fa fa-fw fa-envelope-o" /><span>Messages</span></a>,
-            <a key="3" href=""><i className="fa fa-fw fa-comment-o" /><span>Comments</span></a>,
-            <a key="4" href=""><i className="fa fa-fw fa-bar-chart-o" /><span>Analytics</span></a>,
-            <a key="5" href=""><i className="fa fa-fw fa-newspaper-o" /><span>Reading List</span></a>
-          ];
-  
-        let jsx;
-    
-        jsx = (
-            
-              <a><Menu  >
-                {items}
-              </Menu></a>
-            
-          );
-        return jsx;
-      }
-      
     render() {
-        return (
-            <div className="MainNav">
-            
-            
-                
-                {/*{this.getMenu()}*/}
-            
-            
-           
-                <ul>
-                    <li className="NavOptions"><NavLink to="/home">Home</NavLink></li>
-                    <li className="NavOptions"><NavLink to="/signup">Sign Up</NavLink></li>
-                    <li className="NavOptions"><NavLink to="/login">Log In</NavLink></li>
-                </ul>
-            </div>
-        );
+        return(
+                <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+                    <a className="navbar-brand" href="#">Regimen</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="navbar-collapse collapse dual-collapse">
+                        <ul className="navbar-nav d-flex justify-content-start">
+                            <li className="nav-item">
+                                <NavLink to="/home">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/signup">Sign Up</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/login">Log In</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="navbar-collapse collapse dual-collapse">
+                        <ul className="navbar-nav d-flex justify-content-end">
+                            <li className="nav-item">
+                                <Scrollchor to="#what">What</Scrollchor>
+                            </li>
+                            <li className="nav-item">
+                                <Scrollchor to="#how">How</Scrollchor>
+                            </li>
+                            <li className="nav-item">
+                                <Scrollchor to="#who">Who</Scrollchor>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+        )
     }
 }
+
 export default MainNav;
