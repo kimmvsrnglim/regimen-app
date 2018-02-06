e063591e487018f754fb74e6eb007d18b7d215e7
Kim Lim <kim.lim@Kims-MacBook-Pro.local>
2018-02-06 03:14:12 -0500

    styled form on signup, edited text on about us section

--- client/src/Components/Signup/Signup.js
+++ client/src/Components/Signup/Signup.js
@@ -41,42 +41,47 @@ class Signup extends Component {
         return (
             <Aux>
                 <MainNav/>
-                <div className="container-fluid" id="signupContent">
-                    <form onSubmit={(event) => this.handleSignup(event)}>
-                        <div className="form-row">
-                            <div className="col">
-                                <label htmlFor="username">Email</label>
-                                <input type="text" className="form-control" name="username" placeholder="Email" />
+                <div className="container" id="signupContent">
+                    <div className="SignupTitle">
+                        <h1 className="Greet" align="center">Get Started</h1>
+                    </div>
+                    <div className="SignupForm">
+                        <form onSubmit={(event) => this.handleSignup(event)}>
+                            <div className="form-row">
+                                <div className="col">
+                                    {/* <label htmlFor="username">Email</label> */}
+                                    <input type="text" className="form-control" name="username" placeholder="Email" />
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col">
-                                <label htmlFor="password">Password</label>
-                                <input type="password" className="form-control" name="password" placeholder="Password" />
+                            <div className="form-row">
+                                <div className="col">
+                                    {/* <label htmlFor="password">Password</label> */}
+                                    <input type="password" className="form-control" name="password" placeholder="P A S S W O R D" />
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col">
-                                <label htmlFor="Firstname">First Name</label>
-                                <input type="text" className="form-control" name="firstname" placeholder="First Name" />
+                            <div className="form-row">
+                                <div className="col">
+                                    {/* <label htmlFor="Firstname">First Name</label> */}
+                                    <input type="text" className="form-control" name="firstname" placeholder="First Name" />
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col">
-                                <label htmlFor="Lastname">Last Name</label>
-                                <input type="text" className="form-control" name="lastname" placeholder="Last Name" />
+                            <div className="form-row">
+                                <div className="col">
+                                    {/* <label htmlFor="Lastname">Last Name</label> */}
+                                    <input type="text" className="form-control" name="lastname" placeholder="Last Name" />
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-row">
-                            <div className="col">
-                                <label htmlFor="age">Age</label>
-                                <input type="text" className="form-control" name="age" placeholder="Age" />
+                            <div className="form-row">
+                                <div className="col">
+                                    {/* <label htmlFor="age">Age</label> */}
+                                    <input type="text" className="form-control" name="age" placeholder="Age" />
+                                </div>
                             </div>
-                        </div>
-                        <div className="submit">
-                                <input type="submit" value="submit" /> 
-                            </div>
-                    </form>
+                            <div className="submit">
+                                    <input className="btn btn-outline-dark btn-lg btn-block" type="submit" value="submit" /> 
+                                </div>
+                        </form>
+                    </div>
                 </div>
             </Aux>
         )    
